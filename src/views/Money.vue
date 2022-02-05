7636a9b1e710ff2e4d6cb125c15e124d5762cfd5
Eternal-could <20578466@qq.com>
2022-02-05 14:36:30 +0800

    Money页面布局

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
-    Money.vue
+  <div class="nav-wrapper">
+    <div class="content">
+      Money.vue
+    </div>
     <Nav/>
   </div>
 </template>
@@ -12,5 +14,15 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+  .nav-wrapper{
+    display: flex;
+    border: 1px solid green;
+    flex-direction: column;
+    height: 100vh;
+  }
+  .content{
+    flex-grow: 1;
+    overflow: auto;
+    border: 1px solid blue;
+  }
 </style>
