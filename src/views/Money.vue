d9b54af7f8c6d1b57f58e859dd2becea6b0ce981
Eternal-could <20578466@qq.com>
2022-02-21 13:23:11 +0800

    把record复制保存到list中（深拷贝）

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,7 +1,7 @@
 <template>
   <Layout class-prefix="layout">
     {{record}}
-    <NumberPad :value.sync="record.amount"/>
+    <NumberPad :value.sync="record.amount" @submit="saveRecord"/>
     <Types :value.sync="record.type"/>
     <Notes @update:value="onUpdateNotes"/>
     <Tags :data-source.sync="tags" @update:value="onUpdateTags"/>
@@ -14,7 +14,7 @@ import Types from "@/components/Money/Types.vue";
 import Notes from "@/components/Money/Notes.vue";
 import Tags from "@/components/Money/Tags.vue";
 import Vue from 'vue';
-import {Component, Prop} from 'vue-property-decorator';
+import {Component, Prop, Watch} from 'vue-property-decorator';
 
 type Record = {
   tags: string[],
@@ -28,13 +28,24 @@ type Record = {
 })
 export default class Money extends Vue {
   tags = ['衣','食','住','行'];
-  record: Record = {tags: [], notes: '', type:'-', amount: 0 }
+  record: Record = {tags: [], notes: '', type:'-', amount: 0 };
+  recordList: Record[] = [];
 
   onUpdateTags(value:string[]) {
     this.record.tags = value;
   }
   onUpdateNotes(value: string) {
     this.record.notes = value;
+  }
+  saveRecord() {
+    // 深拷贝
+    const deepClone = JSON.parse(JSON.stringify(this.record))
+    this.recordList.push(deepClone);
+    console.log(this.recordList);
+  }
+  @Watch('recordList')
+  onRecordListChange() {
+    window.localStorage.setItem('recordList', JSON.stringify(this.recordList));
   }
 }
 </script>
