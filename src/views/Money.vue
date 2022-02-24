4ca50fb6a172a7d73bf0191a4685607ac5c89837
Eternal-could <20578466@qq.com>
2022-02-24 21:02:16 +0800

    删除Types.vue

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -13,7 +13,6 @@
 
 <script lang="ts">
 import NumberPad from "@/components/Money/NumberPad.vue";
-import Types from "@/components/Money/Types.vue";
 import FormItem from "@/components/Money/FormItem.vue";
 import Tags from "@/components/Money/Tags.vue";
 import Vue from 'vue';
@@ -39,7 +38,7 @@ import recordTypeList from "@/constants/recordTypeList";
 
 
 @Component({
-  components: {Tabs, Tags, FormItem, Types, NumberPad},
+  components: {Tabs, Tags, FormItem, NumberPad},
 })
 export default class Money extends Vue {
   get recordList(){
